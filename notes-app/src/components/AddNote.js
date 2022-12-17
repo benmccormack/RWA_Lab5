a510ecff9f4cfd0f1bd6cc1c557fc22e191c88f4
benmccormack <c19459424@mytudublin.ie>
2022-12-17 12:13:08 +0000

    add note component created but not yet styled

--- notes-app/src/components/AddNote.js
+++ notes-app/src/components/AddNote.js
@@ -1,1 +1,24 @@
 // here we want to create the form for adding a note.
+import { useState } from "react";
+
+const AddNote = ({addNoteHandler}) => {
+    const [noteText, setNoteText] = useState('');
+    const saveNoteClickHandler = () => {
+        if(noteText.trim().length > 0)
+            addNoteHandler(noteText);
+            setNoteText('');
+    }
+    const handleValueChange = (event) => {
+        setNoteText(event.target.value);
+        console.log(event.target.value);
+    }
+
+    return(<div className="create-note">
+        <h1>Note Buddy</h1>
+        <textarea onChange={handleValueChange} value={noteText} placeholder="Type here to add a new note"></textarea>
+        <input type="color" id="noteColor"/>
+        <button className="create" onClick={saveNoteClickHandler}>Create Note</button>
+    </div> )
+}
+
+export default AddNote;
--- notes-app/src/components/NoteList.js
+++ notes-app/src/components/NoteList.js
@@ -1,10 +1,15 @@
+import AddNote from './AddNote';
 import Note from './Note';
 
 const NotesList = ({notes}) =>{
     return (
-        <div className='notes-list'>
-            {notes.map((note)=> <Note id={note.id} text={note.text}/>)}
+        <div className='container'>
+            <div className='note-create'><AddNote/></div>
+            <div className='notes-list'>
+                {notes.map((note)=> <Note id={note.id} text={note.text}/>)}
+            </div>
         </div>
+
     )
 }
 
